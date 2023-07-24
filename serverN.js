365eb083ce06058cba09344fb500a4d34522685e
Keshab Chaudhary <keshabchaudhary23132@gmail.com>
2023-07-24 15:07:54 +0530

    done

--- serverN.js
+++ serverN.js
@@ -1,16 +1,47 @@
-const http = require('http');
+const express = require("express");//package name must always be written with inverted commas
+const app = express();
+
 const port = 8081;
 
-http
-    .createServer((require, response) => {
-        response.writeHead(200, { "Content-Type": "text/html" });
-        response.write("<h1>Hello,this is being generated from the server itself!</h1>");
-        response.end();
-    })
-    .listen(port, () => {
-        console.log(`Nodejs server started on port ${port}`);
+const toDoList = ["keshab", "chaudhary"];
+app.use(express.json());//this signifies that the data transfer will happen in form of jason file only it can updated as per requirement
+
+app.get("/todos", (req, res) => {
+    res.status(200).send(toDoList);
+});
+
+app.post("/todos", (req, res) => {
+    let newToDoList = req.body.item;
+    toDoList.push(newToDoList);
+    res.status(201).send({
+        message: "The items has been send successfully!",
+    });
+});
+
+app.delete("/todos", (req, res) => {
+    let itemToDelete = req.body.item;
+    toDoList.find((element, index) => {
+        if (element === itemToDelete) {
+            toDoList.splice(index, 1);
+        }
     });
 
-//http://localhost:8081
+    res.status(202).send({
+        message: `${req.body.item} , is the item that has been deleted successfully!`,
+    });
+});
 
-//type this in a browser to know wheatger it is running or not
+//works as else statement all the other methods other than which are implemented here
+app.all("/todos", (req, res) => {
+    res.status(501).send();
+})
+
+
+//will work for all  the other routes other than specified
+app.all("*", (req, res) => {
+    res.status(404).send();
+})
+
+app.listen(port, () => {//this listen function is absically responsible for building up the server
+    console.log(`Nodejs server started on port number :${port}`);
+});
